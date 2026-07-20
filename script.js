b3bbdd32ac94fcb1ba6c11c09d7a958361d0d2ea
kpoovakan <217997837+kpoovakan@users.noreply.github.com>
2026-07-20 10:10:54 -0700

    Update index.html and script.js

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 "use strict";
 /* random stuff saved for later:
 https://www.w3schools.com/html/html5_webstorage.asp
-https://duckduckgo.com?q=pumpkin pie */
+https://duckduckgo.com?q=pumpkin pie
+
+window.addEventListener("keydown", function(event) {
+  if (event.key == "a"){
+        document.querySelector("#result").innerHTML = "You pressed the A key";
+    }
+});
+*/
 
 window.onload = function() {
     document.getElementById("checkJavaScript").remove();
+}
+
+function getTime()) {
+    const d = new Date();
+    let year = d.getFullYear();
+    document.getElementById("date").innerHTML = year;
 }
