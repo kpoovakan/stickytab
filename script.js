ed3a74050dd8b5b84b7250fc578cbe1f6953c774
kpoovakan <217997837+kpoovakan@users.noreply.github.com>
2026-07-19 19:56:48 -0700

    Update index.html and script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use strict";
+/* random stuff saved for later:
+https://www.w3schools.com/html/html5_webstorage.asp
+https://duckduckgo.com?q=pumpkin pie */
+
+window.onload = function() {
+    document.getElementById("checkJavaScript").remove();
+}
