55c43c6ef445661babcac748c92ac92678e7ff5d
kpoovakan <217997837+kpoovakan@users.noreply.github.com>
2026-07-20 10:36:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -21,13 +21,12 @@ function getTime() {
     const d = new Date();
     let year = d.getFullYear();
     let time24 = d.getHours();
-    console.log(time24);
-    if(time24 > 12) {
+    /*if(time24 > 12) {
         let time12 = d.getHours();
         time12 = time12 - 12;
     } else {
         let time12 = time24;
-    } console.log (time12);
+    } console.log (time12);*/
     let timeMinutes = d.getMinutes();
     document.getElementById("date").innerHTML = year;
 }
